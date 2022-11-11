ddf0add325318da4f29fafa2bd850661f2da6d55
LucasJG1994 <lgaraway@yahoo.com>
2022-11-11 07:57:01 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -28,27 +28,6 @@ int main(int argc, char** argv){
 		std::cout << "2nd PASS\n";
 
 		std::cout << "Done...\n";
-	} //else std::cout << "Usage: hasm <file_name>\n";
-
-	yyinit("Release/Pong.asm");
-	parser_init();
-	yydestroy();
-
-	std::cout << "1st PASS\n";
-
-	for (int i = 0; i < ID.size(); i++) {
-		std::cout << "KEY: " << ID[i] << std::endl;
-	}
-
-	second_pass = true;
-
-	yyinit("Release/Pong.asm");
-	parser_init();
-	yydestroy();
-
-	std::cout << "2nd PASS\n";
-
-	std::cout << "Done...\n";
-
+	}else std::cout << "Usage: hasm <file_name>\n";
 	return 0;
 }
